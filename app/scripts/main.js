4b557f6c7f0852c575833539016f002719c1c039
Watanabe Tetsuhiko SFC-01 <tetsu-w.sikisiki.1130@ozzio.jp>
2014-11-02 23:53:32 +0900

    課題中途

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,18 @@
+//ボタンが押されたら実行される関数
+
 var greeting = function(message){
-  window.alert(message);
+  var output=document.querySelector("#answer");
+  output.value=answer;
 };
 
 var helloWorld = function(event){
+  var inputA=document.querySelector("#input-a");
+  var inputB=document.querySelector("#input-b");
+  var a=Number.(inputA.value);
+  var b=Number.(inputB.value);
+  var answer=a+b;
   console.log("helloWorld: start");
-  greeting("Hello, world");
+  greeting(answer);
   console.log("helloWorld: end");
 };
 
@@ -12,6 +20,7 @@ var boot = function(event){
   console.log("boot: start");
   var button = document.querySelector("button");
   button.addEventListener("click", helloWorld);
+  button.addEventListener("dblclick", helloWorld);
   console.log("boot: end");
 };
 
